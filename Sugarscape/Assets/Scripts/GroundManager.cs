fb570166cffbfaab3ceff12a5a567efc7e756f95
Victor Hernández <victorhhg@outlook.com>
2022-02-18 21:27:59 -0600

    Cambios en la implementación del terreno. En lugar de usar una textura con un pixel por celda, se implementó un mapa de Tiles, donde cada Tile puede llevar una textura distinta.

--- Sugarscape/Assets/Scripts/GroundManager.cs
+++ Sugarscape/Assets/Scripts/GroundManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class Grid : MonoBehaviour
+public class GroundManager : MonoBehaviour
 {
     
     public Vector2 gridWorldSize; //Se introducen los valores en el editor.
@@ -12,39 +13,36 @@ public class Grid : MonoBehaviour
     public GameObject peak2Reference;
     public float resourceRadius;
     public int maxResources;
+    public Tilemap tilescape;
+    public TileBase[] sugarTiles;
 
-    Vector2 gridCenter; //Se introducen los valores en el editor.
+    Vector2 gridCenter; 
     Cell[,] grid;
 
     int gridSizeX; //Nos dice el numero de cuadros que caben en el eje X
     int gridSizeY; //Nos dice el número de cuadros que caben en el eje Z. 
-    Texture2D gridTexture;
-    SpriteRenderer rend;
-    
+    int maxSize;
+        
     private void Awake()
     {
         //Se calcula el número de cuadros que caben en el grid dividiendo lo que mide el grid entre el tamaño de las celdas (o lado del cuadrito).
         //Se redondea el resultado a números enteros.
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / cellSize);
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / cellSize);
+        maxSize = gridSizeX * gridSizeY;
+
         gridCenter = transform.position;
-        rend = GetComponent<SpriteRenderer>();
 
         CreateGrid();
         InitiateResources();
     }
 
     void Start(){
-        UpdateSprite();
+        CreateTiles();
+        UnlockTilesColors();
+        UpdateTiles();
     }
 
-    public int MaxSize
-    {
-        get
-        {
-            return gridSizeX * gridSizeY;
-        }
-    }
 
     //Este método crea el grid a partir de las medidas y número de nodos estimados previamente en el Awake.
     void CreateGrid()
@@ -55,7 +53,6 @@ public class Grid : MonoBehaviour
         Vector2 worldBottomLeft = gridCenter - new Vector2(gridWorldSize.x / 2, gridWorldSize.y / 2);
 
         //Se crea un loop doble para iterar en todas las posiciones de los cuadritos en el eje x y el eje y.
-
         for(int x=0; x < gridSizeX; x++)
         { 
             for (int y = 0; y < gridSizeY; y++)
@@ -65,6 +62,9 @@ public class Grid : MonoBehaviour
                 Vector2 worldPoint = worldBottomLeft + new Vector2(x * cellSize + cellSize/2, y * cellSize + cellSize/2);
                 
                 grid[x, y] = new Cell(worldPoint, x, y, 1, 1);
+
+                //Se crea el Tile correspondiente a la celda
+
             }
         }
     } 
@@ -88,6 +88,32 @@ public class Grid : MonoBehaviour
             //Se asigna la capacidad y nivel a la celda en cuestión.
             c.sugarCapacity = sugarCap;
             c.sugarLevel = sugarCap;
+        }
+    }
+
+    private void CreateTiles(){
+        Vector3Int[] positionArray = new Vector3Int[maxSize];
+        TileBase[] tileArray = new TileBase[maxSize];
+        int counter = 0;
+
+        foreach(Cell c in grid){
+            positionArray[counter] = tilescape.WorldToCell(c.worldPosition);
+            tileArray[counter] = sugarTiles[0];
+            counter += 1;
+        }
+
+        tilescape.SetTiles(positionArray, tileArray);
+        
+    }
+
+    private void UpdateTiles(){
+        Vector3Int cellPosition;
+        Color cellColor;
+
+        foreach(Cell c in grid){
+            cellPosition = tilescape.WorldToCell(c.worldPosition);
+            cellColor = SugarTileColor(c.sugarLevel / maxResources);
+            tilescape.SetColor(cellPosition, cellColor);
         }
     }
 
@@ -130,33 +156,26 @@ public class Grid : MonoBehaviour
         return grid[x, y];
     }
 
-    //Método para crear una textura
-    Texture2D CreateTexture(Texture2D tex){
-        foreach (Cell c in grid){
-            Color color = SugarCellColor(c.sugarLevel / maxResources);
-            tex.SetPixel(c.gridX, c.gridY, color);
-        }
-        tex.Apply();
-        tex.filterMode = FilterMode.Point;
-        return tex;
-
-    }
-
-    public void UpdateSprite(){
-        gridTexture = new Texture2D(gridSizeX, gridSizeY);
-        gridTexture = CreateTexture(gridTexture);
-        Sprite gridSprite = Sprite.Create(gridTexture, new Rect(0f,0f,gridTexture.width,gridTexture.height), new Vector2(0.5f, 0.5f),50f);
-        rend.sprite = gridSprite;
-    }
 
     // Función para calcular el color de las celdas basado en la capacidad de azúcar.
-    Color SugarCellColor(float x){
-        Vector3 colorMax = new Vector3(1, 0.8f, 0);
-        Vector3 colorMin = new Vector3(1, 1, 1);
+    Color SugarTileColor(float x){
+        Vector3 colorMax = new Vector3(0.52f, 0.6f, 0f);
+        Vector3 colorMin = new Vector3(1f, 1f, 1f);
         Vector3 newColor = x * colorMax + (1-x) * colorMin;
 
         return(new Color(newColor.x, newColor.y, newColor.z, 1f));
     }
 
+    void UnlockTilesColors(){
+        Vector3Int cellPosition;
+        
+        foreach(Cell c in grid){
+            cellPosition = tilescape.WorldToCell(c.worldPosition);
+            tilescape.RemoveTileFlags(cellPosition, TileFlags.LockColor);
+        }
+    }
 
+
+
+    
 }
